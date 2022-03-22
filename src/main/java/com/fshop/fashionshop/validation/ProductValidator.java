fa1a0c11a120a351111218831ab2d1520ae0b68e
Karen Darbinyan <kar31100@gmail.com>
2022-03-22 14:40:41 +0400

    refactored validation methods some cases

--- src/main/java/com/fshop/fashionshop/validation/ProductValidator.java
+++ src/main/java/com/fshop/fashionshop/validation/ProductValidator.java
@@ -9,10 +9,13 @@ public final class ProductValidator {
     public static boolean validateUpdateProduct(Product product) {
         ImageValidator.checkDefaultImage(product);
 
-        return product.getName().length() != 0 &&
-                product.getPrice() >= 0 &&
-                StockValidator.validateStock(product.getStock()) &&
-                DescriptionValidator.validateDescription(product.getDescription());
+        if (product.getName().length() == 0 ||
+                product.getPrice() < 0 ||
+                !StockValidator.validateStock(product.getStock()) ||
+                !DescriptionValidator.validateDescription(product.getDescription())) {
+            return false;
+        }
+        return true;
 
     }
 
--- src/main/java/com/fshop/fashionshop/validation/commons/ImageValidator.java
+++ src/main/java/com/fshop/fashionshop/validation/commons/ImageValidator.java
@@ -11,6 +11,8 @@ public final class ImageValidator {
         if (product.getImg() == null) {
             product.setImg(new LinkedList<>());
             product.getImg().add(new Image(ValidationConstants.DEFAULT_IMAGE_PATH));
+        } else if (product.getImg().size() == 0) {
+            product.getImg().add(new Image(ValidationConstants.DEFAULT_IMAGE_PATH));
         }
     }
 }
